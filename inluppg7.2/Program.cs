16fb36078216dea6b0d3b2c605a1da675d30bdfc
kebabnekaise <emsi06001@utb.vaxjo.se>
2024-02-02 11:12:31 +0100

    Add project files.

---
+++ inluppg7.2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace inluppg7_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> talList = new List<int>();
+            talList.Add(123);
+            talList.Add(13);
+            talList.Add(99);
+            talList.Add(12);
+
+            Console.WriteLine(Maximum(talList));
+        }
+
+        static int Maximum(List<int> list)
+        {
+            return list.Max();
+        }
+    }
+}
